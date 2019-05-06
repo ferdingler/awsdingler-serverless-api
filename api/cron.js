4c7ea1e4a0020c82299b6aa1d1aea920eb41d2ef
Fernando Dingler <fdingler@amazon.com>
2019-05-05 18:56:14 -0700

    Added cron function to populate k8s dashboard

--- api/cron.js
+++ api/cron.js
@@ -1,22 +1,32 @@
-const Parser = require('rss-parser');
-const _ = require('lodash');
-const parser = new Parser();
+const AWS = require('aws-sdk');
+const health = require('./k8sDashboard/health');
+const moment = require('moment');
 
-exports.exploreAnnouncements = async (event, context) => {
-    console.log('Event', event);
-    const { keywords } = event;
+const s3 = new AWS.S3();
+const exportToS3 = async (results) => {
+    console.log('Uploading results to S3');
+    const upload = await s3.upload({
+        Bucket: 'awsdingler-k8s-dashboard',
+        Key: 'latest.json',
+        Body: JSON.stringify(results),
+    }).promise();
+    console.log('Upload Successful', upload);
+    return upload;
+};
+
+exports.k8sDashboard = async () => {
+    const snapshot = moment();
+    console.log('Snapshot', snapshot.toISOString());
+    let healthResult;
 
     try {
-        const feed = await parser.parseURL('https://aws.amazon.com/new/feed/');
-        feed.items.forEach(announcement => {
-            const title = _.lowerCase(announcement.title);
-            console.log('Title: ', title);
-            console.log('Regex Result: ', regex.exec(title));
+        console.log('Calculating health');
+        healthResult = await health.readDashboardHealth(snapshot);
+        return exportToS3({
+            health: healthResult,
         });
-        return true;
-
-    } catch (e) {
-        console.error('Caca', e);
-        return false;
+    } catch (err) {
+        console.log('Failed to generate Dashboard', err);
+        throw err;
     }
 };
