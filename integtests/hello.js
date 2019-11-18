4ea9ba1683620dce04276819f183980e4346b807
Fernando Dingler <fdingler@amazon.com>
2019-11-18 08:44:35 -0800

    Implemented

--- integtests/hello.js
+++ integtests/hello.js
@@ -1,14 +1,22 @@
 const axios = require('axios');
+const aws = require('aws-sdk');
 const helloLambdaEndpoint = process.env['HELLO_LAMBDA_ENDPOINT'];
 
 exports.handler = async (event) => {
     console.log('Event=', event);
+    const codepipeline = new AWS.CodePipeline();
+    const codePipelineJob = event["CodePipeline.job"];
+
+    if(!codePipelineJob) {
+        return true;
+    }
+
     try {
         const ret = await axios(helloLambdaEndpoint);
         console.log('Successful response', ret);
-        return true;
+        return codepipeline.putJobSuccessResult({ jobId: codePipelineJob.id }).promise();
     } catch (err) {
         console.log('Hello Lambda failed', err);
-        throw err;
+        return codepipeline.putJobFailureResult({ jobId: codePipelineJob.id }).promise();
     }
 };
