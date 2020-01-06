fe875c22f6694779d846d1ece32187438f428a60
Fernando Dingler <fdingler@amazon.com>
2020-01-06 10:56:44 -0800

    Fixing the build

---
+++ .aws-sam/build/HelloIntegrationTests/hello.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+const AWS = require('aws-sdk');
+const helloLambdaEndpoint = process.env['HELLO_LAMBDA_ENDPOINT'];
+
+exports.handler = async (event) => {
+    console.log('Event=', event);
+    const codepipeline = new AWS.CodePipeline();
+    const codePipelineJob = event["CodePipeline.job"];
+
+    if(!codePipelineJob) {
+        return true;
+    }
+
+    try {
+        const ret = await axios(helloLambdaEndpoint);
+        console.log('Successful response', ret);
+        return codepipeline.putJobSuccessResult({ jobId: codePipelineJob.id }).promise();
+    } catch (err) {
+        console.log('Hello Lambda failed', err);
+        return codepipeline.putJobFailureResult({ jobId: codePipelineJob.id }).promise();
+    }
+};
---
+++ .aws-sam/build/KubernetesSpotTerminationHandler/cron.js
@@ -0,0 +1,80 @@
+const AWS = require('aws-sdk');
+const health = require('./k8sDashboard/health');
+const spot = require('./k8sDashboard/spot');
+const watchTower = require('./k8sDashboard/watchtower');
+const moment = require('moment');
+
+const s3Bucket = process.env['RESULTS_S3_BUCKET'];
+const spotTerminationsTable = process.env['SPOT_TERMINATIONS_TABLE'];
+const autoScalingGroupName = process.env['AUTO_SCALING_GROUP_NAME'];
+const uptimeRobotUrl = process.env['UPTIME_ROBOT_URL'];
+const uptimeRobotApiKey = process.env['UPTIME_ROBOT_API_KEY'];
+const uptimeRobotMonitorId = process.env['UPTIME_ROBOT_MONITOR_ID'];
+
+const s3 = new AWS.S3();
+const exportToS3 = async (results) => {
+    console.log('Uploading results to S3');
+    const upload = await s3.upload({
+        Bucket: s3Bucket,
+        Key: 'latest.json',
+        Body: JSON.stringify(results),
+        ACL: 'public-read',
+    }).promise();
+    console.log('Upload Successful', upload);
+    return upload;
+};
+
+exports.k8sDashboard = async () => {
+    const snapshot = moment();
+    console.log('Environment variables');
+    console.log('Snapshot', snapshot.toISOString());
+    console.log('SpotTerminationsTable', spotTerminationsTable);
+    console.log('AutoScalingGroupName', autoScalingGroupName);
+    console.log('UptimeRobotUrl', uptimeRobotUrl);
+    console.log('UptimeRobotApiKey', uptimeRobotApiKey);
+    console.log('UptimeRobotMonitorId', uptimeRobotMonitorId);
+    let healthResult;
+
+    try {
+        console.log('Calculating health');
+        healthResult = await health.readDashboardHealth(snapshot);
+        console.log('Health=', healthResult);
+
+        console.log('Calculating last spot termination');
+        const lastSpotTermination = await spot.lastTermination(spotTerminationsTable);
+        console.log('LastSpotTermination=', lastSpotTermination);
+
+        console.log('Calculating CPU utilization');
+        const cpuMetrics = await spot.cpuAverageUtilization(snapshot, autoScalingGroupName);
+        console.log('CpuMetrics count=', cpuMetrics.length);
+
+        console.log('Listing Spot Instances');
+        const instances = await spot.listSpotInstances(autoScalingGroupName);
+        console.log('Got instances=', instances);
+
+        console.log('Getting latency from watchtower metrics');
+        const latency = await watchTower.latencies(snapshot);
+        console.log('Got latencies', latency);
+
+        console.log('Getting availability from watchtower metrics');
+        const availability = await watchTower.calculateAvailability(
+            snapshot,
+            uptimeRobotUrl,
+            uptimeRobotApiKey,
+            uptimeRobotMonitorId
+        );
+        console.log('Got availability', availability);
+
+        return exportToS3({
+            health: healthResult,
+            lastSpotTermination,
+            cpuMetrics,
+            instances,
+            latency,
+            availability,
+        });
+    } catch (err) {
+        console.log('Failed to generate Dashboard', err);
+        throw err;
+    }
+};
