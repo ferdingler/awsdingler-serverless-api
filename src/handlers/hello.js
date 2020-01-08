7e54bbe9fa8a0be2fcdc6db9677be45640821e97
Fernando Dingler <ferdingler@gmail.com>
2020-01-07 20:41:28 -0800

    Fixing Hello function

--- src/handlers/hello.js
+++ src/handlers/hello.js
@@ -1,15 +1,15 @@
 const hello = require('../hello');
-const buildResponse = require('./buildResponse');
+const utils = require('./buildResponse');
 let response;
 
 exports.handler = async (event) => {
     console.log('Event', event);
     try {
         const helloMessage = await hello.sayHello();
-        response = buildResponse(200, helloMessage);
+        response = utils.buildResponse(200, helloMessage);
     } catch (err) {
         console.log('Error saying hello', err);
-        response = buildResponse(500, helloMessage);
+        response = utils.buildResponse(500, helloMessage);
     }
 
     return response;
