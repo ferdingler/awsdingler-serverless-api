821f8a66d08d78d490856ca53da1bf9a1911cc1a
Fernando Dingler <fdingler@amazon.com>
2020-02-04 11:34:25 -0800

    Removing layer usage from sayHello

--- src/hello/helloService.js
+++ src/hello/helloService.js
@@ -1,9 +1,7 @@
 const axios = require('axios');
-const common = require('/opt/nodejs/algo');
 const url = 'http://checkip.amazonaws.com/';
 
 exports.sayHello = async () => {
-    console.log('Layer = ', common);
     const ret = await axios(url);
     const ip = ret.data.trim();
     const message = {
