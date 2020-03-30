357ec8d252b1e7c4f7943755dfd8c1ad0463872f
Fernando Dingler <fdingler@amazon.com>
2020-03-29 19:37:04 -0700

    Fixing the build

--- pipeline/cdk/lib/pipeline-stack.ts
+++ pipeline/cdk/lib/pipeline-stack.ts
@@ -105,6 +105,7 @@ export class PipelineStack extends cdk.Stack {
 
     // CodeBuild project definition
     const buildProject = new codebuild.PipelineProject(this, 'Build', {
+      badge: true,
       environment: {
         buildImage: codebuild.LinuxBuildImage.AMAZON_LINUX_2_2,
         computeType: codebuild.ComputeType.SMALL
--- src/hello/helloService.js
+++ src/hello/helloService.js
@@ -1,10 +1,16 @@
+/**
+ * X-ray tracing http calls
+ */
 const AWSXRay = require('aws-xray-sdk');
 AWSXRay.captureHTTPsGlobal(require('http'));
-
+AWSXRay.setContextMissingStrategy("LOG_ERROR");
+/**
+ * Import libraries
+ */
 const axios = require('axios');
-const url = 'http://checkip.amazonaws.com/';
 
 exports.sayHello = async () => {
+    const url = 'http://checkip.amazonaws.com/';
     const ret = await axios(url);
     const ip = ret.data.trim();
     const message = {
